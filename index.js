72cd9f12b8aa7c4297289e86ffaa713c71f1f470
Madhur <bhartimadhur21@gmail.com>
2022-12-26 15:43:05 +0530

    Added some layouts

---
+++ index.js
@@ -0,0 +1,20 @@
+function navbarToggle() {
+    console.log("clicked");
+  
+    var element1 = document.getElementById("navBox");
+    element1.classList.toggle("navBox1");
+  
+    var element2 = document.getElementById("mid");
+    element2.classList.toggle("mid1");
+
+    var element3=document.getElementById('super');
+    element3.classList.toggle('super1');
+  }
+  
+  
+  var ele = document.getElementById('animation');
+  ele.style.position='absolute'
+  ele.style.top='10%'
+  ele.style.right='-3%'
+
+
