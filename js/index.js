13b0e92ee2182aca1b3eb22cb19aa2cca7b67217
Madhur <bhartimadhur21@gmail.com>
2023-02-17 21:55:07 +0530

    .

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ function navbarToggle() {
 
   var element3 = document.getElementById("super");
   element3.classList.toggle("super1");
+
 }
 
 
@@ -18,55 +19,63 @@ ele.style.position = "absolute";
 ele.style.top = "10%";
 ele.style.right = "-3%";
 
-//Parallax section begins here
 
-// window.addEventListener("DOMContent")
 
-var sky = document.getElementById("sky");
-var hills = document.getElementById("hills");
-var moon = document.getElementById("moon");
-var mainBox = document.getElementById("main1");
-//lower section
-var lowerBox1 = document.getElementById("main3");
-var lowerBox2 = document.getElementById("main4");
 
-window.addEventListener("scroll", function () {
-  var valueX = this.window.scrollX;
-  var valueY = this.window.scrollY;
-  // setTranslate(0,valueY*0.2*0.2,sky);
-  // setTranslate(0,valueY*0.2,hills);
-  // setTranslate(10,valueY*0.4,moon);
-
-  // console.log(valueX,valueY);
-  sky.style.top = valueY * 0.45 + "px";
-  hills.style.top = valueY * 0.2 + "px";
-  moon.style.left = valueY * 0.15 + "px";
-  // mainBox.style.top = valueY* 0.4 +'px';
-  lowerBox1.style.left = valueY * 0.9 + "px";
-  lowerBox2.style.right = valueY * 0.9 + "px";
-
-  if (window.matchMedia("max-width:830px")) {
-    //   lowerBox1.style.left=valueY*0.4 + 'px';
-    // lowerBox2.style.right=valueY*0.4 + 'px';
-    // console.log('got it')
-  } else if (window.matchMedia("max-width:830px")) {
-    //   lowerBox1.style.left=valueY*0.8 + 'px';
-    // lowerBox2.style.right=valueY*0.8 + 'px';
-  }
-});
-
-// function setTranslate(x,y,ele){
-//   ele.style.transform = "translate3D("+ x +","+ y +"px,0";
-// }
 
 
 //dark mode toggle
-const body = document.querySelector('body');
+const body = document.body
+const hs2 = document.getElementById('h-s-2');
 const main3 = document.getElementById('main3');
+const main4 = document.getElementById('main4');
+
+const navBox = document.getElementById('navBox');
+// const navTxt = document.getElementById('components');
+
+
+
+const out_hs3 = document.getElementById('out-h-s-3');
+const head_hs3 = document.getElementById('head-h-s-3');
+const hs3 = document.getElementById('h-s-3');
+const hs3_txt = document.getElementById('h-s-3-txt');
+
+
+const hs4 = document.getElementById('h-s-4');
+const hoe = document.getElementById('headingOfEvents');
+const out_hs5 = document.getElementById('h-s-5');
+const hs5 = document.getElementById('Teams');
+
+
+
+
 const toggle = document.getElementById('toggle');
 var link = document.getElementById("lnk");
 toggle.onclick=function(){
     toggle.classList.toggle('active');
+    body.classList.toggle('active')
+
+    hoe.classList.toggle('active')
+
+    navBox.classList.toggle('active')
+    // navTxt.classList.toggle('active')
+
+    hs2.classList.toggle('active')
+    main3.classList.toggle('active')
+    main4.classList.toggle('active')
+
+    out_hs3.classList.toggle('active')
+    head_hs3.classList.toggle('active')
+    hs3.classList.toggle('active')
+    hs3_txt.classList.toggle('active')
+
+
+    hs4.classList.toggle('active')
+
+    out_hs5.classList.toggle('active')
+    hs5.classList.toggle('active')
+
+
 
 }
 
